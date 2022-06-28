dbdc533b749ce5a041e90955d0cd029abb00347b
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-06-28 19:24:03 +0400

    initial commit

---
+++ java/HelloWorldCommandLine.java
@@ -0,0 +1,11 @@
+
+public class HelloWorldCommandLine {
+    public static void main(String[] args) {
+        String x = args[0];
+        String y = args[1];
+        // System.out.println("Hello " + x + " and " + y);
+        System.out.printf("Hello %s and %s\n", x, y);
+        System.out.printf("Goodbye %s and %s\n", y, x);
+        // System.out.println("Goodbye " + y + " and " + x);
+    }
+}
