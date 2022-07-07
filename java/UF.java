c770c7e10297622b2125b099c59947c17be872c4
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-08 00:25:37 +0400

    debug uf

--- java/UF.java
+++ java/UF.java
@@ -1,42 +1,59 @@
+import java.util.Arrays;
 
 public class UF {
-
-    private static int id[];
-    private static int count;
+    private int id[];
+    private int count;
 
     public UF(int n) {
         count = n;
         id = new int[n];
+
         for (int i = 0; i < n; i++) {
             id[i] = i;
         }
     }
 
-    public static int count() {
+    public int count() {
         return count;
     }
 
-    public static int find(int i) {
-        int iID;
-        iID = id[i];
-        return iID;
+    public int find(int p) {
+        return id[p];
     }
 
-    public static boolean connected(int p, int q) {
+    public boolean connected(int p, int q) {
         return find(p) == find(q);
     }
 
-    public static void union(int p, int q) {
+    public void union(int p, int q) {
         int pID = id[p];
         int qID = id[q];
-        if (pID == qID)
+
+        if (pID == qID) {
             return;
-        for (int i = 0; i < id.length; i++) {
-            if (id[i] != pID)
-                id[i] = qID;
-            count--;
         }
 
+        for (int i = 0; i < id.length; i++) {
+            if (id[i] == pID) {
+                id[i] = qID;
+            }
+        }
+
+        count--;
     }
 
+    public static void main(String[] args) {
+        UF uf0 = new UF(5);
+        uf0.union(0, 3);
+        System.out.println(uf0.connected(0, 3));
+        System.out.println(uf0.count());
+        uf0.union(3, 4);
+        System.out.println(uf0.connected(0, 4));
+        System.out.println(uf0.count());
+        System.out.println(Arrays.toString(uf0.id));
+        UF uf1 = new UF(3);
+
+        System.out.println(Arrays.toString(uf0.id));
+        System.out.println(Arrays.toString(uf1.id));
+    }
 }
