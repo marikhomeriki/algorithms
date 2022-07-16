7a50c77d9dae29e6208f06e1a37c8c7621c7b539
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-16 19:24:50 +0400

    percolate without optimization

--- java/Percolation.java
+++ java/Percolation.java
@@ -5,10 +5,13 @@ import java.util.*;
 
 public class Percolation {
     private int id[][];
+    private WeightedQuickUnionUF uf;
+    private int openSites;
 
     // creates n-by-n grid, with all sites initially blocked
     public Percolation(int n) {
         id = new int[n][n];
+        uf = new WeightedQuickUnionUF(n * n);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 id[i][j] = 0;
@@ -30,48 +33,77 @@ public class Percolation {
                 if ((row + i > n - 1) || (col + j > n - 1)) {
                     continue;
                 }
-                if (id[row + i][col + j] == 1) {
-                    int k = n * (row + i) + (col + j);
-                    neighbors.add(k);
+                if (id[row + i][col + j] != 1) {
+                    continue;
                 }
-
+                int k = n * (row + i) + (col + j);
+                neighbors.add(k);
             }
         }
-
         return neighbors;
     }
 
     // opens the site (row, col) if it is not open already
     public void open(int row, int col) {
-        if (id[row][col] != 1) {
-            id[row][col] = 1;
+        if (id[row][col] == 1) {
+            return;
         }
+        id[row][col] = 1;
+        ArrayList<Integer> openNeighbors = getOpenNeighbors(row, col);
+        int n = id.length;
+        int k = n * row + col;
+        for (int neighbor : openNeighbors) {
+            uf.union(neighbor, k);
+        }
+        openSites++;
     }
 
     // is the site (row, col) open?
     public boolean isOpen(int row, int col) {
-        return true;
+        return id[row][col] == 1;
     }
 
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
-        return true;
+        int n = id.length;
+        int q = n * row + col;
+        for (int i = 0; i < n; i++) {
+            if (isOpen(0, i)) {
+                if (uf.find(i) == uf.find(q)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     // returns the number of open sites
     public int numberOfOpenSites() {
-        return 1;
+        return openSites;
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        int n = id.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = n * n - n; j < n * n; j++) {
+                if (uf.find(i) == uf.find(j)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     // test client (optional)
     public static void main(String[] args) {
-        Percolation per = new Percolation(10);
-        System.out.println(per.getOpenNeighbors(0, 9));
+        Percolation per = new Percolation(3);
+        per.open(1, 1);
+        per.open(1, 2);
+        per.open(0, 1);
+        per.isFull(0, 1);
+        System.out.println(per.isFull(2, 2));
+        System.out.println(per.percolates());
     }
 
 }
