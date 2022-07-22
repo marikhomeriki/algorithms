81b8fcb647d0b5f110381c66d06588fc267e07f2
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-22 19:43:15 +0400

    Two Sum Faster

---
+++ java/TwoSumAdv.java
@@ -0,0 +1,28 @@
+
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.BinarySearch;
+
+public class TwoSumAdv {
+
+    public static int twosum(int[] a) {
+        Arrays.sort(a);
+        int n = a.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int key = -a[i];
+            int j = BinarySearch.indexOf(a, key);
+            if (i < j) {
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { -1, 3, 1, 4 };
+        System.out.println(twosum(arr));
+
+    }
+}
