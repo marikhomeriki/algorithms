02b5b46b327545ac50987a0ea6761c9b14f4cf80
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-23 16:08:43 +0400

    Advanced Binary Search

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -1,37 +1,40 @@
 
-import java.util.Arrays;
-
 public class BinarySearch {
 
     public static int indexOf(int[] a, int key) {
-        int n = a.length;
-        Arrays.sort(a);
         int lo = 0;
-        int hi = n - 1;
-        int mid = (hi - lo) / 2;
+        int hi = a.length - 1;
 
-        while (key != a[mid]) {
+        while (lo <= hi) {
+            int mid = (lo + hi) / 2;
+
             if (key > a[mid]) {
                 lo = mid + 1;
-                mid = lo + ((hi - lo) / 2);
-                if (mid >= 12) {
-                    return -1;
-                }
-            }
-            if (key < a[mid]) {
+            } else if (key < a[mid]) {
                 hi = mid - 1;
-                mid = (hi - lo) / 2;
-                if (mid <= 0) {
-                    return -1;
-                }
+            } else {
+                return mid;
             }
         }
-        return mid;
+
+        return -1;
     }
 
     public static void main(String[] args) {
         int[] arr = new int[] { 10, 20, 30, 35, 40, 50, 55, 60, 65, 75, 80, 100 };
-        System.out.println(indexOf(arr, -2));
-
+        int[] arr1 = new int[] {};
+        int[] arr2 = new int[] { 1 };
+        System.out.println(indexOf(arr1, 5) == -1);
+        System.out.println(indexOf(arr2, 1) == 0);
+        System.out.println(indexOf(arr2, 3) == -1);
+        System.out.println(indexOf(arr, 5) == -1);
+        System.out.println(indexOf(arr, 15) == -1);
+        System.out.println(indexOf(arr, 30) == 2);
+        System.out.println(indexOf(arr, 50) == 5);
+        System.out.println(indexOf(arr, 80) == 10);
+        System.out.println(indexOf(arr, 120) == -1);
+        System.out.println(indexOf(arr, 10) == 0);
+        System.out.println(indexOf(arr, 100) == 11);
+        System.out.println(indexOf(arr, 90) == -1);
     }
 }
