459b3b53eb39de03296308e0064d8b6e1ab0d4c3
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-23 16:14:02 +0400

    add more test cases

--- java/TwoSumAd.java
+++ java/TwoSumAd.java
@@ -9,14 +9,14 @@ public class TwoSumAd {
         int lo = 0;
         int hi = a.length - 1;
         while (lo < hi) {
-            if (a[lo] + a[hi] == 0) {
-                count++;
-                lo++;
-                hi--;
-            } else if (a[lo] + a[hi] > 0) {
+            if (a[lo] + a[hi] > 0) {
                 hi--;
             } else if (a[lo] + a[hi] < 0) {
                 lo++;
+            } else {
+                count++;
+                lo++;
+                hi--;
             }
         }
         return count;
@@ -26,8 +26,10 @@ public class TwoSumAd {
         int[] arr = new int[] { -1, 3, 1, 4 };
         int[] arr1 = new int[] { -1, 3, 1, 4, 1 };
         int[] arr2 = new int[] { -1, 3, 1, 4, 1, 2, -2, 3, -3, 11 };
-        System.out.println(twoSum(arr));
-        System.out.println(twoSum(arr1));
-        System.out.println(twoSum(arr2));
+        int[] arr3 = new int[] { -1, 3, 4 };
+        System.out.println(twoSum(arr) == 1);
+        System.out.println(twoSum(arr1) == 1);
+        System.out.println(twoSum(arr2) == 3);
+        System.out.println(twoSum(arr3) == 0);
     }
 }
