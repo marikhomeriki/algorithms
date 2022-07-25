cd351ba2ddd816107fd04f8806c1f39e37206787
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-25 14:26:17 +0400

    Implement stack without exception

--- src/week2/LinkedListStack.java
+++ src/week2/LinkedListStack.java
@@ -1,68 +1,36 @@
 
 public class LinkedListStack<Item> {
-    private int[] arr;
-    private int n;
 
-    public LinkedListStack<Item> 
+    public Node first = null;
 
-    public WeightedQuickUnion(int n) {
-        id = new int[n];
-        count = n;
-        for (int i = 0; i < n; i++) {
-            id[i] = i;
-        }
-        sz = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            sz[i] = 1;
-        }
+    private class Node {
+        Item item;
+        Node next;
     }
 
-    public int count() {
-        return count;
+    public boolean isEmpty() {
+        return first == null;
     }
 
-    public int find(int p) {
-        while (p != id[p]) {
-            id[p] = id[id[p]];
-            p = id[p];
-        }
-        return p;
-
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
     }
 
-    public boolean connected(int p, int q) {
-        return find(p) == find(q);
-    }
-
-    public void union(int p, int q) {
-        int pRoot = find(p);
-        int qRoot = find(q);
-
-        if (pRoot == qRoot) {
-            return;
-        }
-
-        if (sz[pRoot] < sz[qRoot]) {
-            id[pRoot] = qRoot;
-            sz[qRoot] += sz[pRoot];
-        } else {
-            id[qRoot] = pRoot;
-            sz[pRoot] += sz[qRoot];
-        }
-        count--;
-
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        return item;
     }
 
     public static void main(String[] args) {
-        WeightedQuickUnion uf0 = new WeightedQuickUnion(5);
-        uf0.union(0, 3);
-        System.out.println(uf0.connected(0, 3));
-        System.out.println(uf0.count());
-        uf0.union(3, 4);
-        System.out.println(uf0.connected(0, 4));
-        System.out.println(uf0.count());
-        System.out.println(uf0.find(3));
-        System.out.println(uf0.find(4));
+        LinkedListStack<Integer> stack = new LinkedListStack<Integer>();
+        stack.push(5);
+        System.out.println(stack.isEmpty());
+        // stack.pop();
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
     }
 }
