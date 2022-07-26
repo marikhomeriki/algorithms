219d3edd998ec87d1af4e58fdfce5ddf3b362b9a
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-26 16:04:19 +0400

    Queue with linked list

---
+++ src/week2/LinkedListQueue.java
@@ -0,0 +1,75 @@
+import java.util.NoSuchElementException;
+
+public class LinkedListQueue<Item> {
+
+    private Node first = null;
+    private Node last = null;
+    private int size = 0;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void enqueue(Item item) {
+        if (size == 0) {
+            last = new Node();
+            first = last;
+            last.item = item;
+            last.next = null;
+        } else {
+            Node oldLast = last;
+            last = new Node();
+            last.item = item;
+            last.next = null;
+            oldLast.next = last;
+        }
+        size++;
+    }
+
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        return first.item;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        Item item = first.item;
+        if (size == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+        }
+        size--;
+        return item;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<Integer>();
+
+        queue.enqueue(1);
+        queue.enqueue(2);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+        queue.enqueue(3);
+        queue.enqueue(4);
+        System.out.println(queue.dequeue());
+        System.out.println(queue.dequeue());
+
+        System.out.println(queue.size());
+        System.out.println(queue.isEmpty());
+    }
+}
