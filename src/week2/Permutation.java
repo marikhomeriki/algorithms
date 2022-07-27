f93b0f7bcee5b280ed01352aa39c2265371a6506
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-27 23:00:26 +0400

    Implement permutation

---
+++ src/week2/Permutation.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdIn;
+
+public class Permutation {
+    public static void main(String[] args) {
+        RandomizedQueue<String> queue = new RandomizedQueue<>();
+        int k = Integer.parseInt(args[0]);
+
+        while (!StdIn.isEmpty()) {
+            String string = StdIn.readString();
+            queue.enqueue(string);
+
+        }
+
+        for (int i = 0; i < k; i++) {
+            System.out.println(queue.sample());
+        }
+
+    }
+}
