0b82bd75c77d2002cbd5fbef96bddfe1dd484ba6
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-07-30 01:56:04 +0400

    Implement shuffle

---
+++ src/week2/Shuffle.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.concurrent.Exchanger;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class Shuffle {
+
+    public static <Item> void shuffle(Item[] a) {
+
+        int n = a.length;
+        int r;
+
+        for (int i = 0; i < n; i++) {
+            r = StdRandom.uniform(i + 1);
+            exch(a, i, r);
+        }
+    }
+
+    public static <Item> void exch(Item[] a, int i, int j) {
+        Item temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+        shuffle(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
