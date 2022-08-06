8806f1d4c2670ad467e36ce93fc284f0a594f1e6
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-06 21:48:58 +0400

    implement three way sort

---
+++ src/week3/ThreeWaySort.java
@@ -0,0 +1,68 @@
+import java.util.Arrays;
+
+import edu.princeton.cs.algs4.StdRandom;
+
+public class ThreeWaySort {
+
+    private static <Item> void exch(Comparable<Item>[] a, int i, int j) {
+
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+
+        if (v.compareTo(w) <= 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private static void sort(Comparable[] a, int lo, int hi) {
+
+        if (lo >= hi) {
+            return;
+        }
+        int i = lo;
+        int lt = lo;
+        int gt = hi;
+
+        Comparable k = a[lo];
+
+        while (i <= gt) {
+
+            int cmp = a[i].compareTo(k);
+
+            if (cmp < 0) {
+                exch(a, lt, i);
+                i++;
+                lt++;
+            } else if (cmp > 0) {
+                exch(a, i, gt);
+                gt--;
+
+            } else {
+                i++;
+            }
+
+        }
+        sort(a, lo, lt - 1);
+        sort(a, lt + 1, hi);
+    }
+
+    public static void sort(Comparable[] a) {
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length - 1);
+    }
+
+    public static void main(String[] args) {
+        Integer[] a = new Integer[] { 8, 4, 3, 9, 10, 7, 6, 1, 11, 2, 5 };
+
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+}
