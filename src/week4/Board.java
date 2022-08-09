20768242882c49331dd66da3698743a508c8fac3
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-10 02:03:31 +0400

    board wip v2

--- src/week4/Board.java
+++ src/week4/Board.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class Board {
 
     private int n;
@@ -8,6 +10,10 @@ public class Board {
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
+
+        if (tiles == null) {
+            throw new IllegalArgumentException();
+        }
 
         n = tiles.length;
         board = new int[n][n];
@@ -82,18 +88,127 @@ public class Board {
 
     // does this board equal y?
     public boolean equals(Object y) {
+        if (y == this) {
+            return true;
+        }
+        if (y == null) {
+            return false;
+        }
+        if (y.getClass() != this.getClass()) {
+            return false;
+        }
 
-        return false;
+        Board board = (Board) y;
+
+        if (this.dimension() != board.dimension()) {
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (this.board[i][j] != board.board[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        ArrayQueue<Board> queue = new ArrayQueue<>();
+        int emptyI = -1;
+        int emptyJ = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    emptyI = i;
+                    emptyJ = j;
+                }
+            }
+        }
+        if (emptyI == -1 || emptyJ == -1) {
+            return queue;
+        }
+
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if ((Math.abs(i) + Math.abs(j)) > 1 || (Math.abs(i) + Math.abs(j)) == 0) {
+                    continue;
+                }
+
+                if ((emptyJ - j) < 0 || (emptyI - i) < 0) {
+                    continue;
+                }
+
+                if ((emptyJ - j) >= n || (emptyI - i) >= n) {
+                    continue;
+                }
+
+                int[][] copy = copy(board);
+
+                swap(copy, emptyI, emptyJ, emptyI - i, emptyJ - j);
+                Board neighbor = new Board(copy);
+                queue.enqueue(neighbor);
+            }
+        }
+
+        return queue;
+    }
+
+    private static int[][] copy(int[][] original) {
+
+        int n = original.length;
+        int[][] copy = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                copy[i][j] = original[i][j];
+            }
+        }
+
+        return copy;
+    }
+
+    private static void swap(int[][] a, int xi, int xj, int yi, int yj) {
+        int temp = a[xi][xj];
+        a[xi][xj] = a[yi][yj];
+        a[yi][yj] = temp;
+
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
+        int[][] twin = copy(board);
+
+        // int xi = StdRandom.uniform(0, n);
+        // int xj = StdRandom.uniform(0, n);
+        // int yi = StdRandom.uniform(0, n);
+        // int yj = StdRandom.uniform(0, n);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int xi = StdRandom.uniform(0, n);
+                int xj = StdRandom.uniform(0, n);
+                int yi = StdRandom.uniform(0, n);
+                int yj = StdRandom.uniform(0, n);
+                if ((xi == yi) && (xj == yj)) {
+                    continue;
+                }
+
+                if (twin[xi][xj] == 0) {
+                    System.out.println("test " + twin[xi][xj]);
+                    continue;
+                }
+                swap(twin, xi, xj, yi, yj);
+
+            }
+        }
+        Board b = new Board(twin);
+        System.out.println(b.toString());
+
         return null;
+
     }
 
     // unit testing (not graded)
@@ -119,18 +234,49 @@ public class Board {
                 { 7, 8, 2 },
 
         };
+        int[][] tils = new int[][] {
+                { 0, 1, 2 },
+                { 3, 4, 5 },
+                { 6, 7, 8 },
+
+        };
+        int[][] tilz = new int[][] {
+                { 2, 1, 0 },
+                { 3, 4, 5 },
+                { 6, 7, 8 },
+
+        };
+        int[][] tilze = new int[][] {
+                { 2, 1, 6 },
+                { 3, 4, 5 },
+                { 0, 7, 8 },
+
+        };
+
+        int[][] tlze = new int[][] {
+                { 3, 5, 1 },
+                { 0, 4, 7 },
+                { 2, 6, 8 },
+
+        };
 
         // for (int i = 0; i < n; i++) {
         // for (int j = 0; j < n; j++) {
         // tiles[i][j] = n * i + j;
         // }
         // }
-        Board board = new Board(tile);
+        Board board = new Board(tlze);
         System.out.println(Arrays.deepToString(tile));
         System.out.println(board.toString());
         System.out.println(board.hamming());
         System.out.println(board.manhattan());
         System.out.println(board.isGoal());
+        System.out.println(board.neighbors());
+        System.out.println(board.twin());
+
+        // for (Board neighbor : board.neighbors()) {
+        // System.out.println(neighbor);
+        // }
 
     }
 
