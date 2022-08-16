372b61784d8b2c598577fce20ccca04230c81c94
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-16 23:25:54 +0400

    Start red black tree

---
+++ src/week5/RedBlackTree.java
@@ -0,0 +1,13 @@
+import javax.management.RuntimeErrorException;
+
+public class RedBlackTree<Key, Value> {
+
+    public Value get(Key key) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+    public void put(Key key, Value value) {
+        throw new RuntimeException("Not Implemented");
+    }
+
+}
