792cbe7fc60655b6793f18afd258766a1752f2ec
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-17 01:50:22 +0400

    Implement Red Black Tree

--- src/week5/RedBlackTree.java
+++ src/week5/RedBlackTree.java
@@ -1,13 +1,102 @@
-import javax.management.RuntimeErrorException;
 
-public class RedBlackTree<Key, Value> {
+public class RedBlackTree<Key extends Comparable<Key>, Value> {
+
+    private static final boolean RED = true;
+    private static final boolean BLACK = false;
+
+    private Node root;
+
+    public class Node {
+        Key key;
+        Value value;
+        Node left;
+        Node right;
+        boolean color;
+
+        public Node(Key key, Value value, Boolean color) {
+            this.key = key;
+            this.value = value;
+            this.color = color;
+        }
+    }
+
+    private void flipColors(Node x) {
+        x.color = RED;
+        x.right.color = BLACK;
+        x.left.color = BLACK;
+    }
+
+    private boolean isRed(Node x) {
+
+        if (x == null) {
+            return false;
+        }
+        return x.color == RED;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = RED;
+        return x;
+    }
 
     public Value get(Key key) {
-        throw new RuntimeException("Not Implemented");
+
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                x = x.left;
+            } else if (cmp > 0) {
+                x = x.right;
+            } else {
+                return x.value;
+            }
+        }
+        return null;
+
     }
 
     public void put(Key key, Value value) {
         throw new RuntimeException("Not Implemented");
+    }
+
+    private Node put(Node h, Key key, Value value) {
+        if (h == null) {
+            return new Node(key, value, RED);
+        }
+
+        int cmp = key.compareTo(h.key);
+        if (cmp < 0) {
+            h = put(h.left, key, value);
+        } else if (cmp > 0) {
+            h = put(h.right, key, value);
+        } else {
+            h.value = value;
+        }
+
+        if (isRed(h.right) && !isRed(h.left)) {
+            h = rotateLeft(h);
+        }
+        if (isRed(h.left) && isRed(h.left.left)) {
+            h = rotateRight(h);
+        }
+        if (isRed(h.left) && isRed(h.right)) {
+            flipColors(h);
+        }
+        return h;
     }
 
 }
