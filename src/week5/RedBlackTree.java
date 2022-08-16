6aa7d42f8e07cbe2a900c6a6a9b8fbb07f99b1eb
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-17 02:29:57 +0400

    Refactor RBT

--- src/week5/RedBlackTree.java
+++ src/week5/RedBlackTree.java
@@ -1,59 +1,28 @@
 
 public class RedBlackTree<Key extends Comparable<Key>, Value> {
 
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
-
     private Node root;
 
-    public class Node {
+    private enum Color {
+        RED,
+        BLACK
+    }
+
+    private class Node {
         Key key;
         Value value;
         Node left;
         Node right;
-        boolean color;
+        Color color;
 
-        public Node(Key key, Value value, Boolean color) {
+        public Node(Key key, Value value, Color color) {
             this.key = key;
             this.value = value;
             this.color = color;
         }
     }
 
-    private void flipColors(Node x) {
-        x.color = RED;
-        x.right.color = BLACK;
-        x.left.color = BLACK;
-    }
-
-    private boolean isRed(Node x) {
-
-        if (x == null) {
-            return false;
-        }
-        return x.color == RED;
-    }
-
-    private Node rotateLeft(Node h) {
-        Node x = h.right;
-        h.right = x.left;
-        x.left = h;
-        x.color = h.color;
-        h.color = RED;
-        return x;
-    }
-
-    private Node rotateRight(Node h) {
-        Node x = h.left;
-        h.left = x.right;
-        x.right = h;
-        x.color = h.color;
-        h.color = RED;
-        return x;
-    }
-
     public Value get(Key key) {
-
         Node x = root;
         while (x != null) {
             int cmp = key.compareTo(x.key);
@@ -66,23 +35,23 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
             }
         }
         return null;
-
     }
 
     public void put(Key key, Value value) {
-        throw new RuntimeException("Not Implemented");
+        root = put(root, key, value);
+        root.color = Color.BLACK;
     }
 
     private Node put(Node h, Key key, Value value) {
         if (h == null) {
-            return new Node(key, value, RED);
+            return new Node(key, value, Color.RED);
         }
 
         int cmp = key.compareTo(h.key);
         if (cmp < 0) {
-            h = put(h.left, key, value);
+            h.left = put(h.left, key, value);
         } else if (cmp > 0) {
-            h = put(h.right, key, value);
+            h.right = put(h.right, key, value);
         } else {
             h.value = value;
         }
@@ -99,4 +68,48 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
         return h;
     }
 
+    private void flipColors(Node x) {
+        x.color = Color.RED;
+        x.right.color = Color.BLACK;
+        x.left.color = Color.BLACK;
+    }
+
+    private boolean isRed(Node x) {
+        if (x == null) {
+            return false;
+        }
+        return x.color == Color.RED;
+    }
+
+    private Node rotateLeft(Node h) {
+        Node x = h.right;
+        h.right = x.left;
+        x.left = h;
+        x.color = h.color;
+        h.color = Color.RED;
+        return x;
+    }
+
+    private Node rotateRight(Node h) {
+        Node x = h.left;
+        h.left = x.right;
+        x.right = h;
+        x.color = h.color;
+        h.color = Color.RED;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        RedBlackTree<Integer, Integer> tree = new RedBlackTree<>();
+
+        int count = 1000 * 1000;
+
+        for (int i = 0; i < count; i++) {
+            tree.put(i, i);
+        }
+
+        for (int i = 0; i < count; i += count / 10) {
+            System.out.println(i + " => " + tree.get(i));
+        }
+    }
 }
