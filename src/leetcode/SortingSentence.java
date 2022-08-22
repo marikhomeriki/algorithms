a46d414b5773c58925bb4a9d003c3f56ea6ef8b3
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-22 22:33:40 +0400

    Sort Sentence

---
+++ src/leetcode/SortingSentence.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class SortingSentence {
+    public String sortSentence(String s) {
+        String[] arr = s.split(" ");
+        String[] res = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            int pos = arr[i].charAt(arr[i].length() - 1) - '0';
+            res[pos - 1] = arr[i].substring(0, arr[i].length() - 1);
+        }
+        return String.join(" ", res);
+    }
+
+    public static void main(String[] args) {
+        SortingSentence ss = new SortingSentence();
+        String input = "is2 sentence4 This1 a3";
+        ss.sortSentence(input);
+        System.out.println(ss.sortSentence(input));
+    }
+}
