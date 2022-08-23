f2f6833fce7d36d9abf14891d553cc994e0391ab
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-24 00:58:36 +0400

    update valid anagrams

--- src/leetcode/ValidAnagram.java
+++ src/leetcode/ValidAnagram.java
@@ -1,27 +1,18 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ValidAnagram {
     private boolean validAnagram(String s, String t) {
-
-        Set<String> mp = new HashSet<>();
 
         char[] ca = s.toCharArray();
         Arrays.sort(ca);
         String key = new String(ca);
 
-        mp.add(key);
-
         char[] ca1 = t.toCharArray();
         Arrays.sort(ca1);
         String key2 = new String(ca1);
 
-        if (mp.contains(key2)) {
-            return true;
-        }
-        return false;
+        return key.equals(key2);
+
     }
 
     public static void main(String[] args) {
