88671838a9be8a860fd412a8433f20603b9442ee
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-23 20:21:42 +0400

    implement height checker

---
+++ src/leetcode/HeightChecker.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class HeightChecker {
+
+    private int heightChecker(int[] heights) {
+        int[] expected = new int[heights.length];
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            expected[i] = heights[i];
+        }
+        Arrays.sort(expected);
+
+        for (int i = 0; i < heights.length; i++) {
+            if (expected[i] != heights[i]) {
+                count++;
+            }
+
+        }
+
+        System.out.println(Arrays.toString(heights));
+        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] heights = new int[] { 1, 1, 4, 2, 1, 3 };
+        HeightChecker hc = new HeightChecker();
+        hc.heightChecker(heights);
+
+    }
+
+}
