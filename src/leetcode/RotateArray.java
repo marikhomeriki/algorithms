e2a961b3a6dfb6aad86f439c9e9414c7903d36a8
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-25 00:44:34 +0400

    Rotate Array

---
+++ src/leetcode/RotateArray.java
@@ -0,0 +1,21 @@
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        int[] arr = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            int j = (i + k) % nums.length;
+            arr[j] = nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = arr[i];
+        }
+    }
+
+    public static void main(String[] args) {
+        RotateArray ra = new RotateArray();
+        int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        ra.rotate(nums, k);
+    }
+}
