97506f26d3ed27e1d75a2cf1e261d552d28beb9b
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-25 23:29:14 +0400

    Smaller thank current

---
+++ src/leetcode/SmallerThanCurrent.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class SmallerThanCurrent {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] results = new int[nums.length];
+        int[] arr = nums.clone();
+        Map<Integer, Integer> map = new HashMap<>();
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            map.put(arr[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            results[i] = map.get(nums[i]);
+        }
+
+        return results;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[] { 8, 1, 2, 2, 3 };
+        SmallerThanCurrent stc = new SmallerThanCurrent();
+        int[] res = stc.smallerNumbersThanCurrent(arr);
+        System.out.println(Arrays.toString(res));
+    }
+
+}
