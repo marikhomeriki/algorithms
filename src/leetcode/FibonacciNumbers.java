4e95940f38978c35eda8a6cf43dd4605cc71e8af
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-08-25 22:06:04 +0400

    Update Fib

--- src/leetcode/FibonacciNumbers.java
+++ src/leetcode/FibonacciNumbers.java
@@ -1,17 +1,28 @@
-public class FibonacciNumbers {
-    public int fib(int n) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class FibonacciNumbers {
+
+    private Map<Integer, Integer> memo = new HashMap<>();
+
+    public int fib(int n) {
         if (n <= 1) {
             return n;
         }
 
-        return fib(n - 1) + fib(n - 2);
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
 
+        int value = fib(n - 1) + fib(n - 2);
+        memo.put(n, value);
+
+        return value;
     }
 
     public static void main(String[] args) {
         FibonacciNumbers fn = new FibonacciNumbers();
-        int fib = fn.fib(2);
+        int fib = fn.fib(4);
         System.out.println(fib);
 
     }
