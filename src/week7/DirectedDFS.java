6b4463e4b562382d332f1076b719c9fcc25e56a7
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-09-09 15:02:10 +0400

    add constructor to dfs

--- src/week7/DirectedDFS.java
+++ src/week7/DirectedDFS.java
@@ -1,3 +1,14 @@
 public class DirectedDFS {
 
+    private boolean[] marked;
+    private int count;
+
+    public DirectedDFS(Digraph G, int s) {
+        marked = new boolean[G.V()];
+        dfs(G, s);
+    }
+
+    private void dfs(Digraph g, int s) {
+    }
+
 }
