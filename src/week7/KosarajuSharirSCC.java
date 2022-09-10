5aabe17975bca4b666ba4c43af6d6d66f1ec44b7
Mariam Khomeriki <khomeriki.mari@gmail.com>
2022-09-10 12:15:33 +0400

    implement kosaraju

--- src/week7/KosarajuSharirSCC.java
+++ src/week7/KosarajuSharirSCC.java
@@ -1,3 +1,43 @@
+import edu.princeton.cs.algs4.DepthFirstOrder;
+import edu.princeton.cs.algs4.Digraph;
+
 public class KosarajuSharirSCC {
+
+    private boolean[] marked;
+    private int[] id;
+    private int count;
+
+    public KosarajuSharirSCC(Digraph G) {
+        marked = new boolean[G.V()];
+        id = new int[G.V()];
+
+        DepthFirstOrder dfs = new DepthFirstOrder(G.reverse());
+
+        for (int w : dfs.reversePost()) {
+            if (!marked[w]) {
+                dfs(G, w);
+                count++;
+            }
+        }
+    }
+
+    private void dfs(Digraph G, int v) {
+        marked[v] = true;
+        id[v] = count;
+
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean stronglyConnected(int v, int w) {
+        return marked[v] == marked[w];
+    }
+
+    public int count() {
+        return count;
+    }
 
 }
